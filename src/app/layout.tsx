6c9daaf054c33c1489879a99f9eaeacfb74711c5
Khuram Niaz <knshaikh@gmail.com>
2024-12-01 23:33:43 -0700

    Fix hydration error casued by ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
-// import { Inter as FontSans } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
+import dynamic from "next/dynamic";
 
-// const fontSans = FontSans({
-//   subsets: ["latin"],
-//   variable: "--font-sans",
-// });
+const ThemeProvider = dynamic(
+  () => import("@/components/theme-provider").then((mod) => mod.ThemeProvider),
+  {
+    ssr: false,
+    loading: () => (
+      // Optional: Add skeleton loader here
+      <div className="min-h-screen bg-background" />
+    ),
+  }
+);
 
 const inter = Inter({
   subsets: ["latin"],
