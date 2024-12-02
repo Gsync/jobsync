91361fab44382dfddf8afbabf39ef0b622506d77
Khuram Niaz <knshaikh@gmail.com>
2024-12-02 11:14:36 -0700

    Fix dynamic server error during build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import dynamic from "next/dynamic";
+import dynamicImport from "next/dynamic";
+export const dynamic = "force-dynamic";
 
-const ThemeProvider = dynamic(
+const ThemeProvider = dynamicImport(
   () => import("@/components/theme-provider").then((mod) => mod.ThemeProvider),
   {
     ssr: false,
