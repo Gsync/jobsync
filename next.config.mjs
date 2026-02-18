428068346a40b855a23ad8775accf34613803e29
Khuram Niaz <knshaikh@gmail.com>
2026-02-18 00:23:59 -0700

    Fix redirect issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-        permanent: true,
-      },
-    ];
-  },
   output: "standalone",
   devIndicators: false,
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 import db from "@/lib/db";
 
 export default async function RootPage() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
   const userCount = await db.user.count();
 
   if (userCount === 0) {
