0d9db9498a30602af6d4f5c2901c1ac0d332a659
Khuram Niaz <knshaikh@gmail.com>
2026-03-31 21:11:08 -0600

    Add recommended security headers

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
   output: "standalone",
   devIndicators: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "DENY" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
